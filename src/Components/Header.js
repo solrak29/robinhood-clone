3038ec62bbdfe3f6b2932e73cd7d6196e584d49b
Carlos <solrak29@yahoo.com>
2020-12-04 17:07:09 -0500

    work in progress

---
+++ src/Components/Header.js
@@ -0,0 +1,28 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import './Header.css'
+
+
+function Header() {
+  return (
+    <div className="Header">
+      <div className="Header-logo">
+        <img src="/assets/logo.svg" alt="Home Logo" />
+      </div>
+      <div className="Header-search">
+        <div className="search-container">
+            < FontAwesomeIcon icon={faSearch} />
+            <input className="search-input" type="text" placeholder='Search'>
+            </input>
+        </div>
+      </div>
+      <div className="Header-links">
+          <a href="#" >FreeStocks</a>
+          <a href="#" >Portfolios</a>
+          <a href="#" >Cash</a>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/Components/Main.js
@@ -0,0 +1,10 @@
+
+function Main() {
+  return (
+    <div className="Main">
+        <p>I am the Main</p>
+    </div>
+  );
+}
+
+export default Main;
